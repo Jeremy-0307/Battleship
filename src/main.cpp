c0abb66fd7302ca6c57a2a35c72a44e7d14a6cb9
Jeremy-0307 <jeremyjem37@gmail.com>
2025-11-16 20:20:18 -0600

    player movement

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,14 @@
 
     #define DEBUG 1
     #define SIZEBOARD 10
-    #define SPACE_COLS 3
+    #define SPACE_COLS 4
     #define SPACE_ROWS 2
     #define EMPTY "."
 
     static std::string boat_board[SIZEBOARD][SIZEBOARD];
     static std::string attack_board[SIZEBOARD][SIZEBOARD];
+
+    int initX, initY;
 
     void setscreen(int* initX, int* initY);
     void initboard();
@@ -23,12 +25,10 @@
 
     void initboard(int* initX, int* initY) {
         setscreen(initX, initY);
-
         for (int column = 0; column <= SIZEBOARD; ++column) {
             for (int row = 0; row <= SIZEBOARD; ++row) {
                 int screenCOL = *initX + (column * SPACE_COLS);
                 int screenROW = *initY + (row * SPACE_ROWS);
-
                 if (row == 0 && column == 0) {
                     continue;
                 }
@@ -56,11 +56,6 @@
 
         *initX = (max_x - total_cols) / 2;
         *initY = (max_y - total_rows) / 2;
-        #if DEBUG
-            mvprintw(5, 5, "max_x[%d]   -   max_y[%d]", max_x, max_y);
-            mvprintw(6, 5, "total_rows[%d]   -   total_cols[%d]", total_rows, total_cols);
-            mvprintw(7, 5, "initX[%d]   -   initY[%d]", *initX, *initY);
-        #endif
     }
 
     void printuser(int* user_x, int* user_y){
@@ -82,17 +77,17 @@
                 (*user_y) -= SPACE_ROWS;
                 break;
         }
-        if (*user_x <= 1){
-            (*user_x) = 2;
+        if (*user_x <= initX){
+            (*user_x) = (initX + SPACE_COLS) - 1;
         }
-        else if (*user_y  <= 1) {
-            (*user_y) = 2;
+        else if (*user_y  <= initY) {
+            (*user_y) = initY + SPACE_ROWS;
         }
-        else if (*user_x >= SIZEBOARD*SPACE_COLS) {
-            (*user_x) = SIZEBOARD*(SPACE_COLS-1);
+        else if (*user_x >= initX + SIZEBOARD * SPACE_COLS) {
+            (*user_x) = (initX + SIZEBOARD * SPACE_COLS) - 1;
         }
-        else if (*user_y  >= SIZEBOARD*SPACE_ROWS) {
-            (*user_y) = SIZEBOARD*(SPACE_ROWS-1);
+        else if (*user_y  >= initY + SIZEBOARD * SPACE_ROWS) {
+            (*user_y) = initY + SIZEBOARD * SPACE_ROWS;
         }
     }
 
@@ -102,20 +97,23 @@
         noecho();
         curs_set(0);
 
-        int initX, initY;
         initboard(&initX, &initY);
-        int user_x = (initX+SIZEBOARD*SPACE_COLS)/2, user_y = (initY+SIZEBOARD*SPACE_ROWS)/2, prev_x = 0, prev_y = 0;
-        int key;
-        do {
-            key = getch();
+        int user_x = (initX + (SIZEBOARD+1)/2 * SPACE_COLS) - 1;
+        int user_y =  initY + (SIZEBOARD+1)/2 * SPACE_ROWS;
+        int prev_x = 0, prev_y = 0, key;
 
+        mvprintw(2, 5, "user_x[%d], user_y[%d]",user_x, user_y);
+        do {
+            refresh();
+            key = getch();
             prev_x = user_x, prev_y = user_y;
             #if DEBUG
-                mvprintw(2, 2, "user_x[%d], user_y[%d]",user_x, user_y);
+                mvprintw(2, 5, "user_x[%d], user_y[%d]",user_x, user_y);
+                //user_x += 1 * SPACE_COLS;
             #endif
+            mvprintw(user_y, user_x, " . ");
             move(&user_x, &user_y, &key);
-            mvprintw(user_y, user_x, "[.]");
-            refresh();
+            mvprintw(user_y, user_x, "[A]");
         } while (key != 'q');
 
         endwin();
